b1e89d873db84b114943ab6843a6eb1ea252e34d
Mahabubul Alam <zikoku07@gmail.com>
2024-10-23 11:33:40 +0600

    add new test

---
+++ src/__checks__/life-rapidfire.check.ts
@@ -0,0 +1,30 @@
+/**
+* This is a Checkly CLI BrowserCheck construct. To learn more, visit:
+* - https://www.checklyhq.com/docs/cli/
+* - https://www.checklyhq.com/docs/cli/constructs-reference/#browsercheck
+*/
+
+import { BrowserCheck, Frequency, RetryStrategyBuilder } from 'checkly/constructs'
+
+new BrowserCheck('life-rapidfire', {
+  name: 'life_rapidfire',
+  activated: true,
+  muted: false,
+  shouldFail: false,
+  runParallel: true,
+  locations: [],
+  tags: [],
+  sslCheckDomain: '',
+  frequency: Frequency.EVERY_10M,
+  environmentVariables: [],
+ // group: your check belongs to group 'SmartFinancial.com',
+  code: {
+    entrypoint: './life-rapidfire.spec.ts',
+  },
+  retryStrategy: RetryStrategyBuilder.linearStrategy({
+    baseBackoffSeconds: 60,
+    maxRetries: 2,
+    maxDurationSeconds: 600,
+    sameRegion: true,
+  }),
+})
